313c32015df56a327f19a74ddba2a82028d90d27
saying <saying121@outlook.com>
2025-05-06 22:15:38 +0800

    init

--- os/src/config.rs
+++ os/src/config.rs
@@ -7,5 +7,5 @@ pub const APP_SIZE_LIMIT: usize = 0x20000;
 #[cfg(feature = "board_k210")]
 pub const CLOCK_FREQ: usize = 403000000 / 62;
 
-#[cfg(feature = "board_qemu")]
+// #[cfg(feature = "board_qemu")]
 pub const CLOCK_FREQ: usize = 12500000;
--- os/src/loader.rs
+++ os/src/loader.rs
@@ -1,3 +1,5 @@
+use core::arch::asm;
+
 use crate::trap::TrapContext;
 use crate::config::*;
 
