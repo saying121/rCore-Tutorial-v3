8857a34b7fd32a3cebc0a2cd1f88bee4c0e4f2b4
saying <saying121@outlook.com>
2025-05-08 21:20:16 +0800

    init

--- os/src/config.rs
+++ os/src/config.rs
@@ -17,5 +17,5 @@ pub const TRAP_CONTEXT: usize = TRAMPOLINE - PAGE_SIZE;
 #[cfg(feature = "board_k210")]
 pub const CLOCK_FREQ: usize = 403000000 / 62;
 
-#[cfg(feature = "board_qemu")]
+// #[cfg(feature = "board_qemu")]
 pub const CLOCK_FREQ: usize = 12500000;
